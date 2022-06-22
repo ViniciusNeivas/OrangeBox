6e122c1304f2d61d6577b1f88d9b37fd4e94e3ad
ViniciusNeivas <91344925+ViniciusNeivas@users.noreply.github.com>
2022-06-22 13:49:00 -0300

    Navbar, Footer e Home

--- src/componentes/estaticos/footer/Footer.tsx
+++ src/componentes/estaticos/footer/Footer.tsx
@@ -8,45 +8,67 @@ function Footer() {
         <>
 
             <Grid container direction="row" justifyContent="center" alignItems="center">
+                
                 <Grid container alignItems="center" item xs={12} direction="row">
 
                     <Grid alignItems="center" item xs={4}>
 
-                        <Box className='box1'>
-
-                            <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-
-                                <Typography variant="h5" align="center" gutterBottom className='textosRedes'>
-
-                                    Quem somos
-
-                                </Typography>
-
-                            </Box>
-
-                        </Box>
-
-                    </Grid>
-
-                    <Grid alignItems="center" item xs={4}>
 
                         <Box className='box1'>
 
                             <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
 
-                                <Typography variant="h5" align="center" gutterBottom className='textosRedes'>
+                                <Typography variant="h5" align="center" gutterBottom >
 
-                                    Institucional
+                                    <p className='titulosFooter textDecoration'>Quem somos</p> 
 
-                                    <Typography variant="h6" align="center" gutterBottom className='textosRedes'>
+                                    <Typography variant="h6" align="center" gutterBottom>
 
-                                    O violeta
+                                       <p className="conteudoFooter">O violeta</p> 
 
                                     </Typography>
 
                                 </Typography>
 
-                                
+                            </Box>
+
+                        </Box>
+
+                    </Grid>
+
+                    <Grid alignItems="center" item xs={4}>
+
+                        <Box className='box1'>
+
+                            <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
+
+                                <Typography variant="h5" align="center" gutterBottom className='titulosFooter'>
+
+                                <p className='titulosFooter textDecoration'>Institucional</p> 
+
+                                    <Typography variant="h6" align="center" gutterBottom>
+
+                                        <p className="conteudoFooter">Como comprar</p> 
+                                        <p className="conteudoFooter">Entregas</p> 
+                                        <p className="conteudoFooter">Política de privacidade</p> 
+                                        <p className="conteudoFooter">Lojas</p>
+                                        <p className="conteudoFooter">Resposabilidade social</p>
+                                        <p className="conteudoFooter">Cashback</p>
+                                        <p className="conteudoFooter">Fornecedor</p>
+                                        <p className="conteudoFooter">Violeta e você</p>
+                                        <p className="conteudoFooter">Trabalhe conosco</p>
+                                        <p className="conteudoFooter">Tabloide de ofertas</p>
+                                        <p className="conteudoFooter">Fale Conosco</p>
+                                        <p className="conteudoFooter">Área do colaborador</p>
+
+
+
+                                    </Typography>
+
+
+                                </Typography>
+
+
 
                             </Box>
 
@@ -59,9 +81,16 @@ function Footer() {
 
                             <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
 
-                                <Typography variant="h5" align="center" gutterBottom className='textosRedes'>
+                                <Typography variant="h5" align="center" gutterBottom className='titulosFooter'>
 
-                                    Formas de pagamento
+                                <p className='titulosFooter textDecoration'>Formas de pagamento</p> 
+
+                                    <Typography variant="h6" align="center" gutterBottom >
+
+                                        <p className="conteudoFooter">Formas de pagamento site</p> 
+                                        <p className="conteudoFooter">Formas de pagamento loja física</p> 
+                                    
+                                    </Typography>
 
                                 </Typography>
 
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,14 +1,125 @@
+import { Box, Card, CardActionArea, CardContent, CardMedia, Grid, makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
 import './Home.css';
 
-function Home (){
+
+function Home() {
+
+    const useStyles = makeStyles({
+        root: {
+            maxWidth: 242,
+        },
+        media: {
+            height: 258,
+            width: 242
+        },
+    });
+    const classes = useStyles();
 
     return (
 
         <>
-        
-        <h1>Home</h1>
-        
+
+            <Grid container direction='row' className='caixa' justifyContent='center' alignItems='center' >
+
+
+            <Grid container style={{marginLeft: "32px"}}>
+          <Box>
+            <Typography style={{color:"#AA836C", fontSize:"25px",marginBottom:"30px", marginTop:"30px",}}variant='h3' className='tituloHome'>Como Funciona</Typography>
+          </Box>
+        </Grid>
+
+                <Grid container justifyContent='center' alignItems='center'>
+
+                    <Grid container  justifyContent='center' alignItems='center' xs={3} item className='cardHome'>
+                        <Card className={classes.root}>
+                            <CardActionArea>
+                                <CardMedia
+                                    className={classes.media} style={{ width: "100px", height: "100px" }}
+                                    image="https://imgur.com/ukiLrIy.jpg"
+                                    title="Lupa"
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="h2" style={{ fontFamily: "jaldi", fontSize: "15px", color: "#785846", fontWeight: "bold" }}>
+                                        Encontre seus produtos favoritos
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p" style={{ fontFamily: "jaldi", fontSize: "px", color: "B6917B" }}>
+                                        Encontre seu produtos favoritos na loja 1
+                                    </Typography>
+
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+
+
+                    <Grid container justifyContent='center' alignItems='center' xs={3} item className='cardHome'>
+                        <Card className={classes.root}>
+                            <CardActionArea>
+                                <CardMedia
+                                    className={classes.media} style={{ width: "100px", height: "100px" }}
+                                    image="https://imgur.com/zUnjvgn.jpg"
+                                    title="Cesta"
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="h2" style={{ fontFamily: "jaldi", fontSize: "15px", color: "#785846", fontWeight: "bold" }}>
+                                        Separamos os produtos para você
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p" style={{ fontFamily: "jaldi", fontSize: "px", color: "B6917B" }}>
+                                        Uma equipe treinada fará a separação da sua compra
+                                    </Typography>
+
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+
+                    <Grid container justifyContent='center' alignItems='center'xs={3} item className='cardHome'>
+                        <Card className={classes.root}>
+                            <CardActionArea>
+                                <CardMedia
+                                    className={classes.media} style={{ width: "100px", height: "100px" }}
+                                    image="https://imgur.com/rSCH4jX.jpg"
+                                    title="Economize"
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="h2" style={{ fontFamily: "jaldi", fontSize: "15px", color: "#785846", fontWeight: "bold" }}>
+                                        Economize tempo e dinheiro
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p" style={{ fontFamily: "jaldi", fontSize: "px", color: "B6917B" }}>
+                                        Retire sua compra na loja ou escolha receber os produtos em sua casa.
+                                    </Typography>
+
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+
+                    <Grid container justifyContent='center' alignItems='center' xs={3} item className='cardHome'>
+                        <Card className={classes.root}>
+                            <CardActionArea>
+                                <CardMedia
+                                    className={classes.media} style={{ width: "100px", height: "100px" }}
+                                    image="https://imgur.com/TksAPBG.jpg"
+                                    title="Carteira"
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="h2" style={{ fontFamily: "jaldi", fontSize: "15px", color: "#785846", fontWeight: "bold" }}>
+                                        Pague suas compras com total segurança
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p" style={{ fontFamily: "jaldi", fontSize: "px", color: "B6917B" }}>
+                                        Clique aqui para verificar as formas de pagamentos
+                                    </Typography>
+
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+
+
+                </Grid>
+
+            </Grid>
         </>
 
 
