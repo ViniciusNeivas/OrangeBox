908960e291b2367cbf398c213664fe8664a3bc22
ViniciusNeivas <91344925+ViniciusNeivas@users.noreply.github.com>
2022-06-22 15:40:55 -0300

    Atualização em Home

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -20,20 +20,20 @@ function Home() {
 
         <>
 
-            <Grid container direction='row' className='caixa' justifyContent='center' alignItems='center' >
+            <Grid container direction='row' justifyContent='center' alignItems='center' xs={12} >
 
 
-            <Grid container style={{marginLeft: "32px"}}>
-          <Box>
-            <Typography variant='h3' className='comoFunciona'>
-                Como Funciona
-            </Typography>
-          </Box>
-        </Grid>
+                <Grid container style={{ marginLeft: "32px" }}>
+                    <Box>
+                        <Typography variant='h3' className='comoFunciona'>
+                            Como Funciona
+                        </Typography>
+                    </Box>
+                </Grid>
 
                 <Grid container justifyContent='center' alignItems='center'>
 
-                    <Grid container  justifyContent='center' alignItems='center' xs={3}>
+                    <Grid container justifyContent='center' alignItems='center' xs={3}>
                         <Card className={classes.root}>
                             <CardActionArea>
                                 <CardMedia
@@ -45,7 +45,7 @@ function Home() {
                                     <Typography gutterBottom variant="h5" component="h2" className="tituloCard">
                                         Encontre seus produtos favoritos
                                     </Typography>
-                                    <Typography variant="body2" color="textSecondary" component="p" className ="subtituloCard">
+                                    <Typography variant="body2" color="textSecondary" component="p" className="subtituloCard">
                                         Encontre seus produtos favoritos na loja 1
                                     </Typography>
 
@@ -67,7 +67,7 @@ function Home() {
                                     <Typography gutterBottom variant="h5" component="h2" className="tituloCard">
                                         Separamos os produtos para você
                                     </Typography>
-                                    <Typography variant="body2" color="textSecondary" component="p" className ="subtituloCard">
+                                    <Typography variant="body2" color="textSecondary" component="p" className="subtituloCard">
                                         Uma equipe treinada fará a separação da sua compra
                                     </Typography>
 
@@ -76,7 +76,7 @@ function Home() {
                         </Card>
                     </Grid>
 
-                    <Grid container justifyContent='center' alignItems='center'xs={3} item className='cardHome'>
+                    <Grid container justifyContent='center' alignItems='center' xs={3} item className='cardHome'>
                         <Card className={classes.root}>
                             <CardActionArea>
                                 <CardMedia
@@ -88,7 +88,7 @@ function Home() {
                                     <Typography gutterBottom variant="h5" component="h2" className="tituloCard">
                                         Economize tempo e dinheiro
                                     </Typography>
-                                    <Typography variant="body2" color="textSecondary" component="p" className ="subtituloCard">
+                                    <Typography variant="body2" color="textSecondary" component="p" className="subtituloCard">
                                         Retire sua compra na loja ou escolha receber em casa.
                                     </Typography>
 
@@ -109,7 +109,7 @@ function Home() {
                                     <Typography gutterBottom variant="h5" component="h2" className="tituloCard">
                                         Pague suas compras com total segurança
                                     </Typography>
-                                    <Typography variant="body2" color="textSecondary" component="p" className ="subtituloCard">
+                                    <Typography variant="body2" color="textSecondary" component="p" className="subtituloCard">
                                         Clique aqui para verificar as formas de pagamentos
                                     </Typography>
 
@@ -120,6 +120,26 @@ function Home() {
 
 
                 </Grid>
+
+
+                <Grid container direction='row' xs={12} >
+
+                    <Box>
+
+                    <Typography variant="h1" color="initial" className="pergunta">
+
+                        Perguntas?
+
+                    </Typography>
+
+
+                    </Box>
+
+
+                </Grid>
+
+
+
 
             </Grid>
         </>
